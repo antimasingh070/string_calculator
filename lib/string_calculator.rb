c3d92b4c2c4d03dd47a1ec99b3c79bcbbe09674b
Antima Singh <santima2290@gmail.com>
2025-04-27 03:34:10 +0530

    feat: Handle single number input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
+# StringCalculator is a utility to sum numbers from a string input.
 class StringCalculator
-  def self.add(_numbers)
-    0
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+    numbers.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,22 @@
+# frozen_string_literal: true
+
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
-  describe ".add" do
-    context "when input is an empty string" do
-      subject { described_class.add("") }
+  describe '.add' do
+    context 'when input is an empty string' do
+      subject { described_class.add('') }
 
       it { is_expected.to eq(0) }
+    end
+
+    context 'with single number', :single_number do
+      it 'returns the number itself' do
+        aggregate_failures do
+          expect(described_class.add('5')).to eq(5)
+          expect(described_class.add('10')).to eq(10)
+        end
+      end
     end
   end
 end
